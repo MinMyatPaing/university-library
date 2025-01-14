742a76e101756e0fb0ec8a9d8117680b38b63eb9
MinMyatPaing <minmyatpaing64@gmail.com>
2025-01-14 20:02:44 +0000

    Theme set up and basic header implementation

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import BookList from "@/components/home/BookList";
+import BookOverview from "@/components/home/BookOverview";
+
+const Home = () => {
+  return (
+    <>
+      <BookOverview />
+      <BookList />
+    </>
+  );
+};
+
+export default Home;
